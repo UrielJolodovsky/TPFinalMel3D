175d93074066d0394a3ec9e23f4f538d5077c4be
ThiagoBarraza <46645966@est.ort.edu.ar>
2022-11-14 19:41:27 -0300

    ssss

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    int Health;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Health <= 0)
+        {
+            Destroy(gameObject);    
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Ammo"))
+        {
+            Health--;
+        }
     }
 }
