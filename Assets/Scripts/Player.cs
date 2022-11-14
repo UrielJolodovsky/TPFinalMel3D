2fc80427bcda8e5bcb29291c014739d148b8622d
Jeremias_Abulafia <jeremiasabulafia@gmail.com>
2022-11-14 20:04:34 -0300

    el insano post processing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] public float Health;
     public float ActualHealth;
-    
-    
+    [SerializeField] GameObject Sangre;
+    [SerializeField] PostProcessVolume post;
+    [SerializeField] Vignette vignet;
+
     // Start is called before the first frame update
     void Start()
     {
         ActualHealth = Health;
+        Sangre = GameObject.FindGameObjectWithTag("post");
+        post = Sangre.GetComponent<PostProcessVolume>();
+        post.profile.TryGetSettings(out vignet);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        vignet.intensity.value = -ActualHealth;
     }
 
     private void OnTriggerStay(Collider collision)
@@ -25,6 +31,7 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy"))
         {
             ActualHealth--;
+            
         }
     }
 }
