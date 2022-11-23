c39a07615aa7ba9b4a2b68e8c3c7a259c88983ab
Uriel Jolodosvky <46960341@est.ort.edu.ar>
2022-11-22 22:31:12 -0300

    Singleton added

---
+++ Assets/Scripts/GuardarGameobject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardarGameobject : MonoBehaviour
+{
+    public static GuardarGameobject posicion;
+    void Awake()
+    {
+        if (GuardarGameobject.posicion == null)
+        {
+            GuardarGameobject.posicion = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
